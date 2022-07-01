9555a2caaaae5e1cbca5ab620509d0719fd23c16
chaedol2 <chs891117@gmail.com>
2022-07-02 08:52:57 +0900

    modify: pom.xml, PhotogramStartApplication.java

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -2,8 +2,10 @@ package com.cos.photogramstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.cos.photogramstart"})
 public class PhotogramStartApplication {
 
 	public static void main(String[] args) {
