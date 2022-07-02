146e99611dc1fbea705b6eb4bb2b4a67098ae4d5
chaedol2 <chs891117@gmail.com>
2022-07-02 16:27:07 +0900

    add pom.xml

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -2,12 +2,12 @@ package com.cos.photogramstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@ComponentScan({"com.cos.photogramstart"})
 public class PhotogramStartApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogramStartApplication.class, args);
 	}
